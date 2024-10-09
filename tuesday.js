29db7af3254657f48b33e1d802121d67779a2265
Etisyai <etisyaip@gmail.com>
2024-10-09 09:48:00 -0400

    compl

---
+++ tuesday.js
@@ -0,0 +1,41 @@
+console.log("TUESDAY")
+
+n = 12
+let x = 123;
+
+i = 123
+f = 99.999
+
+num = 1.1 
+
+text = "123.45 a words"
+
+number =  parseFloat (text)
+
+double = number * 2
+console.log(double)
+
+ d = new Date()
+ console.log(d)
+ console.log(d.getTime())
+
+ let counter = 0
+ counter++ 
+ counter++
+ console.log(counter)
+ counter--
+ console.log(counter)
+
+ let level = 0
+ level += 5
+ level += 5
+ level += 5
+ console.log(level)
+ level -= 5
+ console.log(level)
+
+ console.log(3+5)
+ console.log(3-5)
+ console.log(3*5)
+ console.log(3/5)
+ 
