e813ecec59e4e87bac4c0742acaba72df0eaf75c
Etisyai <etisyaip@gmail.com>
2024-10-11 10:29:50 -0400

    compl

---
+++ area/area.js
@@ -0,0 +1,21 @@
+//area
+
+// height = 10
+// width = 20
+// area = height * width 
+// console.log(area);
+
+function area(width,height){
+    const resultArea = width * height;
+    return resultArea;
+}
+
+console.log(area(12,20));
+console.log(area(12,20));
+console.log(area(24,40));
+
+height = 10
+width = 20
+myArea = area(width, height)
+console.log(myArea)
+
---
+++ counter/counter.js
@@ -0,0 +1,29 @@
+// counter.js
+
+value = 0
+
+value++; // ++ increment by one
+value++;//  ++ increment by one 
+
+console.log(value)  //expecting 2
+
+beers = 99
+beers--
+beers--
+beers--
+beers--
+console.log(beers) 
+
+counterValue = 0
+incrementAmount = 5
+
+counterValue += incrementAmount
+counterValue += incrementAmount
+counterValue += incrementAmount
+counterValue += 5
+counterValue += 5
+
+console.log(counterValue)
+
+current = 0
+console.log(current++)
